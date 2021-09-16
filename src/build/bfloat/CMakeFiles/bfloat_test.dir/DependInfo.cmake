0d5b6ae84ce64003e64e75af96358580502a6f76
Julius Isken <julius.isken@googlemail.com>
2021-09-16 12:40:30 +0200

    added Catch as submodule

---
+++ src/build/bfloat/CMakeFiles/bfloat_test.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jolles/Documents/MasterDataScience/Masterarbeit/Convergence_bfloat16/src/bfloat/bfloat.test.cpp" "/home/jolles/Documents/MasterDataScience/Masterarbeit/Convergence_bfloat16/src/build/bfloat/CMakeFiles/bfloat_test.dir/bfloat.test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../submodules/Catch2/src/catch2/.."
+  "../bfloat"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/jolles/Documents/MasterDataScience/Masterarbeit/Convergence_bfloat16/src/build/submodules/Catch2/src/CMakeFiles/Catch2WithMain.dir/DependInfo.cmake"
+  "/home/jolles/Documents/MasterDataScience/Masterarbeit/Convergence_bfloat16/src/build/bfloat/CMakeFiles/bfloat.dir/DependInfo.cmake"
+  "/home/jolles/Documents/MasterDataScience/Masterarbeit/Convergence_bfloat16/src/build/submodules/Catch2/src/CMakeFiles/Catch2.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
