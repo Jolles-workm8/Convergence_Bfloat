c89b057e334c51984987354541bcf7478fb5c216
Julius Isken <julius.isken@googlemail.com>
2021-09-16 12:45:59 +0200

    removed buidl artifacts

--- src/build/io/CMakeFiles/io.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libio.a"
-  "libio.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/io.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
